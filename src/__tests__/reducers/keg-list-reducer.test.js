0ee572f8c90f532ba5e6fa7caeed82207a4dbb50
Chelsea Becker <63199870+cschweig2@users.noreply.github.com>
2021-02-19 10:42:29 -0800

    test 4 FAIL

--- src/__tests__/reducers/keg-list-reducer.test.js
+++ src/__tests__/reducers/keg-list-reducer.test.js
@@ -2,8 +2,44 @@ import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
 
+    let action;
+
+    const kegData = {
+      name: 'Flower Power',
+      brand: 'Dr. Spice',
+      flavor: 'Chamomile and Hibiscus',
+      price: 90,
+      pintsLeft: 124,
+      id: 1
+    }
+
   test('Should return default state if no action type is recognized', () => {
     expect(kegListReducer({}, { type: null })).toEqual({});
+  });
+
+  test('Should successfully add new keg to masterKegList', () => {
+    const { name, brand, flavor, price, id } = kegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      flavor: flavor,
+      price: price,
+      pintsLeft: 124,
+      id: id
+    };
+
+    expect(kegListReducer({}, action)).toEqual({
+      [id] : {
+        name: name,
+        brand: brand,
+        flavor: flavor,
+        price: price,
+        pintsLeft: 124,
+        id: id
+      }
+    });
+
   });
 
 });
--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,8 +1,17 @@
 export default (state = {}, action) => {
-  // const { name, brand, flavor, price, pintsLeft, id } = action;
+  const { name, brand, flavor, price, id } = action;
   switch (action.type) {
     case 'ADD_KEG':
-      return state;
+      return Object.assign({}, state, {
+        [id] : {
+          name: name,
+          brand: brand,
+          flavor: flavor,
+          price: price,
+          pintsLeft: 124,
+          // id: id
+        }
+      });
     default:
       return state;
   }
