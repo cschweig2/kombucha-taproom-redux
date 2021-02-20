dcf15b49b72542106f62bf7b7b5a7a0b05163aba
Chelsea Becker <63199870+cschweig2@users.noreply.github.com>
2021-02-19 16:38:31 -0800

    test 14 FAIL

---
+++ src/__tests__/reducers/selected-keg-reducer.test.js
@@ -0,0 +1,10 @@
+import selectedKegReducer from '../../reducers/selected-keg-reducer';
+import * as c from '../../actions/ActionTypes';
+
+describe('selectedKegReducer', () => {
+
+  test('Should return default state if no action type is recognized', () => {
+    expect(selectedKegReducer(false, { type: null })).toEqual(false);
+  });
+
+});
