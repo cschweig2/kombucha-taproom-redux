657ffa140dec4c25072782bab02ec803bce8fb8a
Chelsea Becker <chelraebecker@gmail.com>
2021-02-23 20:09:59 -0800

    test 17 PASS

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -1,22 +1,23 @@
 import * as actions from './../../actions';
+import * as c from './../../actions/ActionTypes';
 
 describe('kombucha taproom actions', () => {
   it('deleteKeg should create DELETE_KEG action', () => {
     expect(actions.deleteKeg(1)).toEqual({
-      type: 'DELETE_KEG',
+      type: c.DELETE_KEG,
       id: 1
     });
   });
 
   it('toggleForm should create TOGGLE_FORM action', () => {
     expect(actions.toggleForm()).toEqual({
-      type: 'TOGGLE_FORM'
+      type: c.TOGGLE_FORM,
     });
   });
 
   it('addKeg should create ADD_KEG action', () => {
     expect(actions.addKeg({name: 'Flower Power', brand: 'Dr. Spice', flavor: 'Chamomile and Hibiscus', price: 90, pintsLeft: 124, id: 1})).toEqual({
-      type: 'ADD_KEG',
+      type: c.ADD_KEG,
       name: 'Flower Power',
       brand: 'Dr. Spice',
       flavor: 'Chamomile and Hibiscus',
@@ -25,5 +26,11 @@ describe('kombucha taproom actions', () => {
       id: 1
     });
   });
+
+  it('toggleEdit should create TOGGLE_EDIT action', () => {
+    expect(actions.toggleEdit()).toEqual({
+      type: c.TOGGLE_EDIT
+    })
+  })
 
 });
